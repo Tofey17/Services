30f584004a53fa79eae571b27b330a1e5fa5482e
علش <153824248+nesrx@users.noreply.github.com>
2024-01-02 23:01:33 +0300

    Update start_msg.py

--- plugins/start_msg.py
+++ plugins/start_msg.py
@@ -36,7 +36,7 @@ async def startm(app, msg):
             [btn(text='⦅ قسم الخدمات ⦆', callback_data='service')],
             [btn(text='⦅ تجميع الرصيد ⦆', callback_data='invite'), btn(text='⦅ شراء الرصيد ⦆', callback_data='buy')],
             [btn(text='⦅ معلومات حسابك ⦆', callback_data='account'), btn(text='⦅ تحويل الرصيد ⦆', callback_data='trans')],
-            [btn(text='قناة التحديثات', url='ui_xb.t.me')]
+            [btn(text='قناة التحديثات', url='http://t.me/xLxLxLrr3')]
         ]
     )
         rk = f'''
